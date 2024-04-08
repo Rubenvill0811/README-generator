45bfc491326bc4087ff0f184490fd6a48e8a346c
Rubenvill0811 <152895707+Rubenvill0811@users.noreply.github.com>
2024-04-07 21:56:23 -0700

    Update response-compiler.js
    Finished writing my the logic needed to take the responses and fill in a README.md template with template literals. In this update I also added badges.

--- response-compiler.js
+++ response-compiler.js
@@ -3,7 +3,46 @@ const fs = require("fs");
 
 function docCompiler(answer) {
     return `
-    This document was written by ${answer.author}
+# ${answer.title}       
+
+## Description
+${answer.description}
+
+### License
+![Static Badge](https://img.shields.io/badge/License-${answer.license}-white?style=plastic)
+
+
+
+## Table of Contents
+- Installation
+- Dependancies
+- Usage
+- Contribution
+- Tests
+- Contact me
+
+## Installation
+${answer.installation}
+
+### Depedencies 
+${answer.dependencies}
+
+## Usage
+${answer.usage}
+## Contribution
+${answer.contribution}
+## Tests
+${answer.tests}
+# Contact Me 
+### GitHub
+![Static Badge](https://img.shields.io/badge/GitHub-${answer.github}-white?style=for-the-badge&logo=GitHub)
+
+### Email me
+![Static Badge](https://img.shields.io/badge/Email-${answer.emailUser}%40${answer.emailPro}-white?style=for-the-badge&logo=gmail)
+
+### Find me on LinkedIn
+![Static Badge](https://img.shields.io/badge/LinkedIn-${answer.linkedin}-white?style=for-the-badge&logo=LinkedIn)
+
     
     
     `
